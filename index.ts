f38f6532e957cfba306281b3f28dbebd99b2811a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-13 07:59:12 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,21 +2,24 @@ console.clear();
 import { from, fromEvent } from 'rxjs';
 import { sequenceEqual, map, bufferCount, mergeMap, tap } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/conditional/sequenceequal
+
 const expectedSequence = from(['q', 'w', 'e', 'r', 't', 'y']);
-const setResult = text =>
-  document.getElementById('result').innerText = text;
+const setResult = (text) =>
+  (document.getElementById('result').innerText = text);
 
 fromEvent(document, 'keydown')
   .pipe(
     map((e: KeyboardEvent) => e.key),
-    tap(v => setResult(v)),
+    tap((v) => setResult(v)),
     bufferCount(6),
-    mergeMap(keyDowns =>
-      from(keyDowns)
-        .pipe(
-          sequenceEqual(expectedSequence),
-          tap(isItQwerty =>
-            setResult(isItQwerty ? 'WELL DONE!' : 'TYPE AGAIN!'))
-        ))
+    mergeMap((keyDowns) =>
+      from(keyDowns).pipe(
+        sequenceEqual(expectedSequence),
+        tap((isItQwerty) =>
+          setResult(isItQwerty ? 'WELL DONE!' : 'TYPE AGAIN!')
+        )
+      )
+    )
   )
-  .subscribe(e => console.log(`did you say qwerty? ${e}`));
+  .subscribe((e) => console.log(`did you say qwerty? ${e}`));
