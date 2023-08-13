ef0038205e31bef5ebdf80317c7875cfa9017ce3
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-13 07:57:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-sequenceEqual-mergeMap-bufferCount-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-sequenceequal-buffercount-eskr8f)
---
+++ index.ts
@@ -0,0 +1,22 @@
+console.clear();
+import { from, fromEvent } from 'rxjs';
+import { sequenceEqual, map, bufferCount, mergeMap, tap } from 'rxjs/operators';
+
+const expectedSequence = from(['q', 'w', 'e', 'r', 't', 'y']);
+const setResult = text =>
+  document.getElementById('result').innerText = text;
+
+fromEvent(document, 'keydown')
+  .pipe(
+    map((e: KeyboardEvent) => e.key),
+    tap(v => setResult(v)),
+    bufferCount(6),
+    mergeMap(keyDowns =>
+      from(keyDowns)
+        .pipe(
+          sequenceEqual(expectedSequence),
+          tap(isItQwerty =>
+            setResult(isItQwerty ? 'WELL DONE!' : 'TYPE AGAIN!'))
+        ))
+  )
+  .subscribe(e => console.log(`did you say qwerty? ${e}`));
